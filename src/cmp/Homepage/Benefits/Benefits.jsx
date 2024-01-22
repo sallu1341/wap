f6da07b62963cf9770a478c181eb3f091dc8c8a0
salman <salmanshaikh8500@gmail.com>
2024-01-22 23:11:56 +0530

    notes1

--- src/cmp/Homepage/Benefits/Benefits.jsx
+++ src/cmp/Homepage/Benefits/Benefits.jsx
@@ -1,9 +1,59 @@
 import React from 'react'
 import "./Benefits.css"
+import {Col ,Row ,Container} from "react-bootstrap";
+
+const data=[{icon:"fa fa-check",
+title:"Sketck Heift",
+desc:"qwertyuiopasdfghjklzxcvb"},
+
+{icon:"fa fa-edit",
+title:"Sketck Heift",
+desc:"qwertyuiopasdfghjklzxcvb"},
+
+{icon:"fa fa-share",
+title:"Sketck Heift",
+desc:"qwertyuiopasdfghjklzxcvb"},]
 
 const Benefits = () => {
+
+    const List=(data)=>{
+      return (
+        <>
+        <div className='d-flex hero mb-4 '>
+          <div>
+            <div  className="icon-box d-flex justify-content-center align-items-center">
+            <i className={data.Catdata.icon} ></i>
+</div>
+            </div>
+            <div className='px-3 '>
+              <h5>{data.Catdata.title}</h5>
+              <p className="w-25"> {data.Catdata.desc}
+              </p>
+            </div>
+         
+          </div>
+
+        </>
+      )
+    }
   return (
-   <><h1>Welcome benefits</h1></>
+    <Container>
+      <Row >
+        <Col md className='  px-4 d-flex flex-column align-items-center justify-content-center'>
+
+        {
+          data.map((item)=>{
+            return <List Catdata={item}/>
+          })
+        }
+          
+        </Col>
+        <Col  md className=' md px-4'>
+        <img  src="man.jpg" className='rounded-circle  man-pic' width="80%"/>
+        </Col>
+      </Row>
+    </Container>
+  
   )
 }
 
--- src/cmp/Homepage/Category/Category.jsx
+++ src/cmp/Homepage/Category/Category.jsx
@@ -1,24 +1,41 @@
 import React from 'react'
 import "./Category.css"
-import { Col,Container,Row } from 'react-bootstrap'
+import { Col,Container,Row ,Card ,} from 'react-bootstrap'
+import CategoryData from "../../../json-api/Category"
 
 
-const data=["q","s","ef","ef"]
-const Colum=()=>{
+
+const Colum=(data)=>{
   return <>
-    <Col></Col>
+    <Col md className='px-4 mt-4 mt-md-0'>
+    <Card style={{ width: '13rem' }}>
+      
+      <Card.Body className='text-center py-4'>
+      <div className="icon d-flex justify-content-center align-items-center ">
+        <i className={data.catData.icon} style={{fontSize:"50px"}}></i>
+      </div>
+        <Card.Title>{data.catData.title}</Card.Title>
+        <Card.Text>
+       {data.catData.desc}
+        </Card.Text>
+    
+      </Card.Body>
+    </Card>
+
+
+    </Col>
   </>
 }
  
 const Category = () => {
   return (
    <>
-   <Container>
+   <Container className='cat-box'>
     <Row>
       
       {
-      data.map((item)=>{
-        return <Colum />
+      CategoryData.map((item)=>{
+        return <Colum  catData={item} id={item.id}/>
 
       })
       }
